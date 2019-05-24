1814f184932dedbaf78bad0d6cddd049821e98f6
Fernando da Silva Rosa <fernandosvrosa@ymail.com>
2019-05-24 09:50:52 -0300

    create project

---
+++ src/main/java/br/com/quake/loganalyze/LogAnalyzeApplication.java
@@ -0,0 +1,13 @@
+package br.com.quake.loganalyze;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LogAnalyzeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LogAnalyzeApplication.class, args);
+	}
+
+}
